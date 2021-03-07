ca52fe485290f47732db8ca01ee2c580152547f3
André Luís <hell.andrew1@gmail.com>
2021-03-07 10:17:44 -0300

    adicionado formulário de login

---
+++ estoqueVB/estoqueVB/Frm_Login.Designer.vb
@@ -0,0 +1,148 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_Login))
+        Me.lbl_email = New System.Windows.Forms.Label()
+        Me.lbl_senha = New System.Windows.Forms.Label()
+        Me.txt_email = New System.Windows.Forms.TextBox()
+        Me.txt_senha = New System.Windows.Forms.TextBox()
+        Me.lbl_info = New System.Windows.Forms.Label()
+        Me.btn_login = New System.Windows.Forms.Button()
+        Me.btn_cancelar = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lbl_email
+        '
+        Me.lbl_email.AutoSize = True
+        Me.lbl_email.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_email.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_email.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lbl_email.Location = New System.Drawing.Point(55, 30)
+        Me.lbl_email.Name = "lbl_email"
+        Me.lbl_email.Size = New System.Drawing.Size(45, 18)
+        Me.lbl_email.TabIndex = 0
+        Me.lbl_email.Text = "Email"
+        '
+        'lbl_senha
+        '
+        Me.lbl_senha.AutoSize = True
+        Me.lbl_senha.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_senha.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_senha.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lbl_senha.Location = New System.Drawing.Point(50, 68)
+        Me.lbl_senha.Name = "lbl_senha"
+        Me.lbl_senha.Size = New System.Drawing.Size(50, 18)
+        Me.lbl_senha.TabIndex = 1
+        Me.lbl_senha.Text = "Senha"
+        '
+        'txt_email
+        '
+        Me.txt_email.Location = New System.Drawing.Point(118, 30)
+        Me.txt_email.Name = "txt_email"
+        Me.txt_email.Size = New System.Drawing.Size(168, 23)
+        Me.txt_email.TabIndex = 2
+        '
+        'txt_senha
+        '
+        Me.txt_senha.Location = New System.Drawing.Point(118, 63)
+        Me.txt_senha.Name = "txt_senha"
+        Me.txt_senha.Size = New System.Drawing.Size(168, 23)
+        Me.txt_senha.TabIndex = 3
+        '
+        'lbl_info
+        '
+        Me.lbl_info.AutoSize = True
+        Me.lbl_info.BackColor = System.Drawing.Color.Transparent
+        Me.lbl_info.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lbl_info.ForeColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lbl_info.Location = New System.Drawing.Point(12, 175)
+        Me.lbl_info.Name = "lbl_info"
+        Me.lbl_info.Size = New System.Drawing.Size(32, 18)
+        Me.lbl_info.TabIndex = 4
+        Me.lbl_info.Text = "Info"
+        '
+        'btn_login
+        '
+        Me.btn_login.BackColor = System.Drawing.Color.Transparent
+        Me.btn_login.FlatAppearance.BorderSize = 0
+        Me.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_login.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_login.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn_login.Image = CType(resources.GetObject("btn_login.Image"), System.Drawing.Image)
+        Me.btn_login.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btn_login.Location = New System.Drawing.Point(105, 101)
+        Me.btn_login.Name = "btn_login"
+        Me.btn_login.Size = New System.Drawing.Size(75, 55)
+        Me.btn_login.TabIndex = 5
+        Me.btn_login.Text = "Login"
+        Me.btn_login.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_login.UseVisualStyleBackColor = False
+        '
+        'btn_cancelar
+        '
+        Me.btn_cancelar.BackColor = System.Drawing.Color.Transparent
+        Me.btn_cancelar.FlatAppearance.BorderSize = 0
+        Me.btn_cancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_cancelar.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btn_cancelar.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.btn_cancelar.Image = CType(resources.GetObject("btn_cancelar.Image"), System.Drawing.Image)
+        Me.btn_cancelar.ImageAlign = System.Drawing.ContentAlignment.TopCenter
+        Me.btn_cancelar.Location = New System.Drawing.Point(211, 101)
+        Me.btn_cancelar.Name = "btn_cancelar"
+        Me.btn_cancelar.Size = New System.Drawing.Size(75, 54)
+        Me.btn_cancelar.TabIndex = 6
+        Me.btn_cancelar.Text = "Cancelar"
+        Me.btn_cancelar.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        Me.btn_cancelar.UseVisualStyleBackColor = False
+        '
+        'Frm_Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(373, 202)
+        Me.Controls.Add(Me.btn_cancelar)
+        Me.Controls.Add(Me.btn_login)
+        Me.Controls.Add(Me.lbl_info)
+        Me.Controls.Add(Me.txt_senha)
+        Me.Controls.Add(Me.txt_email)
+        Me.Controls.Add(Me.lbl_senha)
+        Me.Controls.Add(Me.lbl_email)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Frm_Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lbl_email As Label
+    Friend WithEvents lbl_senha As Label
+    Friend WithEvents txt_email As TextBox
+    Friend WithEvents txt_senha As TextBox
+    Friend WithEvents lbl_info As Label
+    Friend WithEvents btn_login As Button
+    Friend WithEvents btn_cancelar As Button
+End Class
